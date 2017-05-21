418e36e9cfc1b2a882e3066e52e7aab292119a62
Roshan <rcroshan63@gmail.com>
2017-05-21 22:24:57 +0545

    used twitch api

--- resources/app.js
+++ resources/app.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-	
+$( document ).ready( function() {
+	var streamers = [ "freecodecamp", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas" ];
+
+	$.each( streamers, function( i, streamer ) {
+		var url = "https://wind-bow.glitch.me/twitch-api/streams/" + streamer;
+		$.ajax({
+			url: url,
+			dataType: 'jsonp',
+			success: function( response ) {
+				console.log(response);
+			}
+		});
+	});
 });
