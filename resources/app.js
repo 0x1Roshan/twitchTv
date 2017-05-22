570af8b33c463b19a24fe9ee54b74c5bf3d7e50e
Roshan <rcroshan63@gmail.com>
2017-05-22 20:04:16 +0545

    updated layout

--- resources/app.js
+++ resources/app.js
@@ -18,14 +18,22 @@ function putContent( streamer, response )
 {
 	var status;
 	var html;
+	var cls;
+	var logo;
 
 	if ( response.stream == null ) {
 		status = "offline";
+		cls = "alert alert-danger";
+		logo = "https://image.freepik.com/free-vector/old-television-set-icon_23-2147501665.jpg";
 	} else {
 		status = "online";
+		cls = "alert alert-success";
+		logo = response.channel.logo ;
 	} 
 
-	html = "<p>" + streamer + status + "</p>";
+	html = "<div class='" + cls + "'>" +
+				"<img src='" + logo + "' height='50' width='50'/>" + 
+				"<p>" + streamer + status + "</p></div>" ;
 
 	$("#list").append( html );
 }
