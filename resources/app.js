1fd90cc5df8463d2f070b6cc751026d959d54ed4
Roshan <rcroshan63@gmail.com>
2017-05-30 21:47:33 +0545

    added extra information if channel is online

--- resources/app.js
+++ resources/app.js
@@ -7,7 +7,6 @@ $( document ).ready( function() {
 			url: url,
 			dataType: 'jsonp',
 			success: function( response ) {
-				// console.log(response);
 				putContent( streamer, response );
 			}
 		});
@@ -48,18 +47,24 @@ function putContent( streamer, response )
 	var cls;
 	var logo;
 	var name;
+	var game;
+	var channelStatus;
 
 	if ( response.stream === null ) {
 		status = "offline";
 		cls = "alert alert-danger";
 		logo = "http://simpleicon.com/wp-content/uploads/tv.png";
 		name = streamer;
+		description = "";
 
 	} else {
 		status = "online";
 		cls = "alert alert-success";
 		logo = response.stream.channel.logo;
 		name = response.channel ? response.channel.display_name : streamer;
+		game = response.stream.channel.game;
+		channelStatus = response.stream.channel.status;
+		description = "(" + game + " : " + channelStatus + " )";
 	}
 
 	var channelUrl = "https://www.twitch.tv/" + streamer;
@@ -67,7 +72,7 @@ function putContent( streamer, response )
 	html = "<div class='row'>" +
 						"<div class='" + cls + "'>" +
 							"<div class='channel-img'><img src='" + logo + "' class='img-rounded'/></div>" +
-							"<div class='channel-name'>" + name + "</div><a href='" + channelUrl +"' target='_blank'><div class='channel-status'>"+ status + "</div></a>" +
+							"<div class='channel-name'>" + name + description + "</div><a href='" + channelUrl +"' target='_blank'><div class='channel-status'>"+ status + "</div></a>" +
 						"</div>" +
 					"</div>"  ;
 
